c32a4f40fbcae92014d7d30f8d3ce1372a63faac
Dufour <Dufour@DESKTOP-5452IJF>
2022-03-15 16:20:28 +0100

    Polymorphism

---
+++ instructions/src/instructions/Decrement.java
@@ -0,0 +1,10 @@
+package instructions;
+
+public class Decrement extends Instruction {
+	
+	int r;
+	
+	public Decrement(int r) {
+		this.r = r;
+	}
+}
---
+++ instructions/src/instructions/Jump.java
@@ -0,0 +1,8 @@
+package instructions;
+
+public class Jump extends Instruction {
+	
+	int a;
+	
+	public Jump(int a) { this.a = a; }
+}
